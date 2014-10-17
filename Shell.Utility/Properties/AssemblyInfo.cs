66b67bdaaed030936cfbc3b4ef4c9e2a9c1f6fd8
Sam Harwell <sam@tunnelvisionlabs.com>
2014-10-16 20:26:09 -0500

    Update Shell.Utility and Text.Utility to only have a single assembly targeting VS 2010+. Rely on binding redirection for it to work in extensions limited to newer releases.

--- Shell.Utility/Properties/AssemblyInfo.cs
+++ Shell.Utility/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tvl.VisualStudio.Shell.Utility")]
+[assembly: AssemblyTitle("Tvl.VisualStudio.Shell.Utility.10")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
-[assembly: AssemblyProduct("Tvl.VisualStudio.Shell.Utility")]
+[assembly: AssemblyProduct("Tvl.VisualStudio.Shell.Utility.10")]
 [assembly: AssemblyCopyright("Copyright © Sam Harwell 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
