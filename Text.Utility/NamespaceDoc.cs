35ea9e0299c209bad7f9f1c721f1855e5e346016
Sam Harwell <sam@tunnelvisionlabs.com>
2014-11-09 20:30:04 -0600

    Add missing namespace documentation

---
+++ Text.Utility/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+﻿namespace Tvl.VisualStudio.Text
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The <see cref="Tvl.VisualStudio.Text"/> namespaces define interfaces and classes to simplify development of
+    /// Visual Studio extensions which interact with the text editor.
+    /// </summary>
+    [CompilerGenerated]
+    internal static class NamespaceDoc
+    {
+    }
+}
