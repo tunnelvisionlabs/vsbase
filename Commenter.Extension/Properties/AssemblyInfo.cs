f46b6da371fc67bcec83e67a51228486de784a50
Sam Harwell <sam@tunnelvisionlabs.com>
2017-01-25 09:08:53 -0600

    Use ProvideCodeBaseAttribute instead of ProvideBindingPathAttribute

--- Commenter.Extension/Properties/AssemblyInfo.cs
+++ Commenter.Extension/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using Tvl.VisualStudio.Commenter;
+using Microsoft.VisualStudio.Shell;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -35,7 +35,11 @@ using Tvl.VisualStudio.Commenter;
 [assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyInformationalVersion("1.1.0")]
 
-[ProvideBindingPath]
-internal class BindingPathClass
-{
-}
+[assembly: ProvideCodeBase(
+    AssemblyName = "Tvl.VisualStudio.Text.Commenter.Implementation",
+    Version = "1.0.0.0",
+    CodeBase = "$PackageFolder$\\Tvl.VisualStudio.Text.Commenter.Implementation.dll")]
+[assembly: ProvideCodeBase(
+    AssemblyName = "Tvl.VisualStudio.Text.Commenter.Interfaces",
+    Version = "1.0.0.0",
+    CodeBase = "$PackageFolder$\\Tvl.VisualStudio.Text.Commenter.Interfaces.dll")]
