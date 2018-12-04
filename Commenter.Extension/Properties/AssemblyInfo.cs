b6cfc80050e2a4d3e5354b87f455df09be626c2e
Sam Harwell <sam@tunnelvisionlabs.com>
2018-12-04 17:12:45 -0600

    Update assembly info for release 1.4.0

--- Commenter.Extension/Properties/AssemblyInfo.cs
+++ Commenter.Extension/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using Microsoft.VisualStudio.Shell;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0-dev")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
 
 [assembly: ProvideCodeBase(
     AssemblyName = "Tvl.VisualStudio.Text.Commenter.Implementation",
